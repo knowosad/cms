0d40885e3e6590b21654f81c3217a23471965df6
knowosad <k.nowosad@hotmail.com>
2017-12-23 16:45:40 +0100

    get all cinemas

---
+++ src/main/java/pl/com/bottega/cms/app/CinemaFinder.java
@@ -0,0 +1,7 @@
+package pl.com.bottega.cms.app;
+
+/**
+ * Created by freszczypior on 2017-12-23.
+ */
+public interface CinemaFinder {
+}
---
+++ src/main/java/pl/com/bottega/cms/app/dtos/CinemaDto.java
@@ -0,0 +1,7 @@
+package pl.com.bottega.cms.app.dtos;
+
+/**
+ * Created by freszczypior on 2017-12-23.
+ */
+public class CinemaDto {
+}
