4c3b81038b2bb40f49c9b875cc2274984be94e0a
knowosad <k.nowosad@hotmail.com>
2018-02-03 20:36:27 +0100

    implemented getMovieShows at given date

--- src/main/java/pl/com/bottega/cms/ui/controllers/CinemaController.java
+++ src/main/java/pl/com/bottega/cms/ui/controllers/CinemaController.java
@@ -1,12 +1,16 @@
 package pl.com.bottega.cms.ui.controllers;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import pl.com.bottega.cms.app.CinemaFinder;
 import pl.com.bottega.cms.app.CommandGateway;
+import pl.com.bottega.cms.app.MovieFinder;
 import pl.com.bottega.cms.app.dtos.CinemaDto;
+import pl.com.bottega.cms.app.dtos.MovieDto;
 import pl.com.bottega.cms.model.commands.CreateCinemaCommand;
 import pl.com.bottega.cms.model.commands.CreateShowsCommand;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -16,12 +20,13 @@ import java.util.List;
 public class CinemaController {
 
     private CommandGateway gateway;
+    private MovieFinder movieFinder;
+    private CinemaFinder cinemaFinder;
 
-    private CinemaFinder finder;
-
-    public CinemaController(CommandGateway gateway, CinemaFinder finder) {
+    public CinemaController(CommandGateway gateway, MovieFinder movieFinder, CinemaFinder finder) {
         this.gateway = gateway;
-        this.finder = finder;
+        this.movieFinder = movieFinder;
+        this.cinemaFinder = finder;
     }
 
     @PutMapping("/cinemas")
@@ -31,12 +36,17 @@ public class CinemaController {
 
     @GetMapping("/cinemas")
     public List<CinemaDto> getAll(){
-        return finder.getAll();
+        return cinemaFinder.getAll();
     }
 
     @PutMapping("/cinemas/{cinemaId}/shows")
     public void createShows(@PathVariable Long cinemaId, @RequestBody CreateShowsCommand cmd){
         cmd.setCinemaId(cinemaId);
         gateway.execute(cmd);
+    }
+    @GetMapping("cinemas/{cinemaId}/movies")
+    public List<MovieDto> getShows(@PathVariable Long cinemaId,
+                                   @RequestParam @DateTimeFormat(pattern = "yyyy/MM/dd") LocalDate date){
+        return movieFinder.search(cinemaId, date);
     }
 }
